d6b2c42a9419add5a208c4289d8a91f094da0071
Kamil Foltyński <tazzpl@gmail.com>
2018-06-30 21:30:24 +0200

    updates

--- R/save_restore.R
+++ R/save_restore.R
@@ -1,63 +1,87 @@
-
-
-if (FALSE) {
-  
-  # temp
-  # dir.create(tmp.dir <- tempfile())
-  # on.exit(unlink(tmp.dir, TRUE, TRUE))
-  # cache <- cachemer$new(file.path(tmp.dir, "config.yaml"))
-  # cache$clear()
-  # for (i in 1:22)
-  #   res %c-% testFun(a = 1:22, b = i, c = list(d = i / 2, e = i/3))
-  # ============================
-  
-  library(future)
-  
-  saveCache <-
-    function(x = NULL,
-             plan = "multiprocess",
-             workers = future::availableCores() - 1,
-             promises.env) {
-      
+#' saveCache
+#'
+#' Save object to rds
+#'
+#' @param x 
+#' @param plan 
+#' @param workers 
+#' @param promises.env 
+#' @param logger.name 
+#'
+#' @importFrom future %<-% resolve resolved futureOf plan
+#'
+#' @return
+#' @export
+#'
+saveCache <-
+  function(x,
+           path,
+           promises.env,
+           sufix,
+           prefix = "cachemer",
+           plan = "multiprocess",
+           workers = future::availableCores() - 1,
+           logger.name = "test.logger",
+           future.plan = "multiprocess") {
     
-    # quote regardin to saving cache to file
+    # stopifnot(!missing(x))
+    stopifnot(!missing(x))
+    stopifnot(!missing(path))
+    stopifnot(!missing(promises.env))
+    
+    if (missing(sufix))
+      sufix <- as.numeric(Sys.time())
+    
+    fname <- paste0(prefix, "_", sufix, ".rds")
+    
+    stopifnot(!file.exists(file.path(path, fname)))
+    
+    # quote regarding to saving cache to file
+    promises.env$fname <- fname
+    promises.env$path <- path
+    promises.env$x <- x
+    
     qt <- quote({
       fresult %<-% {
-        cat("Resolving...")
-        Sys.sleep(10)
-        cat("DONE")
-        
-        cat(rnorm(1, 1, 4), "\n", file = "dev/save.log", append = TRUE)
-        
+        saveRDS(x, file = file.path(path, fname))
       }
     })
     
+    # future of future variable
     fof <- tryCatch({
       future::futureOf(promises.env$fresult)
     }, error = function(e) {
-      print("'fresult' does not exists")
+      flog.debug("'fresult' does not exists", name = logger.name)
       NULL
     })
     
     if (is.null(fof)) {
       # if does not exists then procees and create
+      flog.debug("Evaluating promise...", name = logger.name)
       eval(qt, envir = promises.env)
+      FALSE
     } else {
       # exists so if not resolved then wait
       fof <- future::futureOf(promises.env$fresult)
       
       if (!future::resolved(fof)) {
+        flog.debug("Wait until previous process is finished...",
+                   name = logger.name)
         future::resolve(fof)
-        
+        flog.debug("Done", name = logger.name)
+        TRUE
       } else {
+        flog.debug("Evaluating promise...", name = logger.name)
         eval(qt, envir = promises.env)
+        FALSE
       }
-      
     }
-    
   }
+
+if (FALSE) {
   promises.env <- new.env()
+  var <- list(a = 1, b = 2, c = list(e=2, 241234))
   debugonce(saveCache)
-  saveCache(promises.env = promises.env)
+  res <- saveCache(var, path = "dev/", promises.env = promises.env)
   
 }
